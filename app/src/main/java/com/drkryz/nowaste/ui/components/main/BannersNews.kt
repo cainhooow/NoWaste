0bdd8fd2efbef874738ecc78a3198adbd4e71a84
drpidman <augustocaio663@gmail.com>
2023-12-02 20:49:09 -0300

    Refactor

---
+++ app/src/main/java/com/drkryz/nowaste/ui/components/main/BannersNews.kt
@@ -0,0 +1,74 @@
+package com.drkryz.nowaste.ui.components.main
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.drkryz.nowaste.R
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.yield
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun BannersNews() {
+
+    val pagerState = rememberPagerState(initialPage = 0);
+    val imageSlider = listOf(
+        painterResource(id = R.drawable.presentation_adicione_s_receita),
+        painterResource(id = R.drawable.presentation_adicione_s_receita2)
+    )
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            yield()
+            delay(2600)
+            pagerState.animateScrollToPage(
+                page = (pagerState.currentPage) % (pagerState.pageCount)
+            )
+        }
+    }
+
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HorizontalPager(
+            count = imageSlider.size,
+            state = pagerState,
+            modifier = Modifier.fillMaxWidth()
+        ) { page ->
+            Image(
+                painter = imageSlider[page],
+                contentDescription = "presentation_image",
+                Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .clip(
+                        RoundedCornerShape(10.dp)
+                    )
+            )
+        }
+
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            activeColor = MaterialTheme.colorScheme.primary,
+            modifier = Modifier
+                .padding(top = 15.dp)
+        )
+    }
+}
