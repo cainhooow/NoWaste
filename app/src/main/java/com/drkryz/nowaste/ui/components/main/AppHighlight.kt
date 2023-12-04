b6eeed23c2cecdd62a4f10a2430d994198a209f7
drpidman <augustocaio663@gmail.com>
2023-12-04 14:19:15 -0300

    Estilização da seção "Destaques" AppHighlight.kt

--- app/src/main/java/com/drkryz/nowaste/ui/components/main/AppHighlight.kt
+++ app/src/main/java/com/drkryz/nowaste/ui/components/main/AppHighlight.kt
@@ -21,6 +21,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.drkryz.nowaste.HomeUI
 import com.drkryz.nowaste.R
@@ -32,12 +34,14 @@ fun AppHighlightHeader() {
     Box(
         Modifier
             .padding(top = 20.dp, start = 25.dp)
-            .clip(
-                RoundedCornerShape(
-                    topStartPercent = 40,
-                    topEndPercent = 40,
-                    bottomStartPercent = 0,
-                    bottomEndPercent = 0
+            .background(
+                color = if (!isSystemInDarkTheme()) Light_background_color
+                else MaterialTheme.colorScheme.onBackground,
+                shape = RoundedCornerShape(
+                topStartPercent = 40,
+                topEndPercent = 40,
+                bottomStartPercent = 0,
+                bottomEndPercent = 0
                 )
             )
             // .background(MaterialTheme.colorScheme.onBackground)
@@ -46,7 +50,11 @@ fun AppHighlightHeader() {
                 end = 8.dp
             )
     ) {
-        Text(text = "Destaques")
+        Text(
+            text = "Destaques",
+            color = if (!isSystemInDarkTheme()) MaterialTheme.colorScheme.onBackground
+            else MaterialTheme.colorScheme.background
+        )
     }
 }
 
@@ -55,7 +63,7 @@ fun AppHighlights() {
     Column(
         Modifier
             .fillMaxWidth()
-            .padding(start = 15.dp, end = 15.dp, top = 10.dp)
+            .padding(start = 15.dp, end = 15.dp)
     ) {
         AppCardPost()
     }
@@ -66,7 +74,7 @@ fun AppHighlights() {
 fun AppCardPost() {
     Column(
         Modifier
-            .padding(top = 15.dp, bottom = 15.dp)
+            .padding(bottom = 30.dp)
             .background(
                 color = if (!isSystemInDarkTheme()) Light_background_color
                 else MaterialTheme.colorScheme.onBackground,
@@ -75,66 +83,86 @@ fun AppCardPost() {
             .padding(start = 10.dp, end = 10.dp, top = 15.dp, bottom = 15.dp)
             .fillMaxWidth()
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.presentation_adicione_s_receita),
-                contentDescription = null,
-                modifier =
-                Modifier
-                    .clip(RoundedCornerShape(50.dp))
-                    .width(61.dp)
-                    .height(61.dp),
-                contentScale = ContentScale.Crop
+        CardMeta()
+        CardBody()
+    }
+}
+
+@Composable
+fun CardMeta() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.presentation_adicione_s_receita),
+            contentDescription = null,
+            modifier =
+            Modifier
+                .clip(RoundedCornerShape(50.dp))
+                .width(61.dp)
+                .height(61.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Column {
+            Text(
+                text = "User.name",
+                fontSize = TextUnit(18f, TextUnitType.Sp),
+                modifier = Modifier.padding(start = 10.dp),
+                color = if (!isSystemInDarkTheme()) MaterialTheme.colorScheme.onBackground
+                else MaterialTheme.colorScheme.background
             )
 
             Text(
-                text = "User.name",
-                Modifier.padding(start = 10.dp),
+                text = "3 minutos atrás",
+                fontSize = TextUnit(13f, TextUnitType.Sp),
+                modifier = Modifier.padding(start = 10.dp),
+                color = if (!isSystemInDarkTheme()) MaterialTheme.colorScheme.onBackground
+                else MaterialTheme.colorScheme.background
+            )
+        }
+    }
+}
+
+@Composable
+fun CardBody() {
+    Column(
+        Modifier
+            .padding(top = 15.dp)
+    ) {
+        Column(
+            Modifier
+                .padding(start = 10.dp)
+        ) {
+            Text(
+                text = "Bolo de chocolate com morango",
+                color = if (!isSystemInDarkTheme()) MaterialTheme.colorScheme.onBackground
+                else MaterialTheme.colorScheme.background
+            )
+            Text(
+                text = "Ingredientes para o bolo:\n" +
+                        "\n" +
+                        "2 xícaras de farinha de trigo\n" +
+                        "1 xícara de cacau em pó\n" +
+                        "2 xícaras de açúcar\n" +
+                        "2 colheres de chá de fermento em pó\n" +
+                        "1 colher de chá de bicarbonato de sódio\n" +
+                        "1 colher de chá de sal\n.....",
                 color = if (!isSystemInDarkTheme()) MaterialTheme.colorScheme.onBackground
                 else MaterialTheme.colorScheme.background
             )
         }
 
-        Column(
+        Image(
+            painter = painterResource(id = R.drawable.presentation_adicione_s_receita),
+            contentDescription = null,
             Modifier
                 .padding(top = 15.dp)
-        ) {
-            Column(
-                Modifier
-                    .padding(start = 10.dp)
-            ) {
-                Text(
-                    text = "Bolo de chocolate com morango",
-                    color = if (!isSystemInDarkTheme()) MaterialTheme.colorScheme.onBackground
-                    else MaterialTheme.colorScheme.background
+                .fillMaxWidth()
+                .clip(
+                    RoundedCornerShape(10.dp)
                 )
-                Text(
-                    text = "Ingredientes para o bolo:\n" +
-                            "\n" +
-                            "2 xícaras de farinha de trigo\n" +
-                            "1 xícara de cacau em pó\n" +
-                            "2 xícaras de açúcar\n" +
-                            "2 colheres de chá de fermento em pó\n" +
-                            "1 colher de chá de bicarbonato de sódio\n" +
-                            "1 colher de chá de sal\n.....",
-                    color = if (!isSystemInDarkTheme()) MaterialTheme.colorScheme.onBackground
-                    else MaterialTheme.colorScheme.background
-                )
-            }
-
-            Image(
-                painter = painterResource(id = R.drawable.presentation_adicione_s_receita),
-                contentDescription = null,
-                Modifier
-                    .padding(top = 15.dp)
-                    .fillMaxWidth()
-                    .clip(
-                        RoundedCornerShape(10.dp)
-                    )
-            )
-        }
+        )
     }
 }
 
